c66a70244bf65bdfbcd25c001fc2bb13d38afb55
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-04-29 11:14:55 +0200

    deletion of unnecessary documents and files

--- example/src/App.js
+++ example/src/App.js
@@ -9,33 +9,28 @@ const App = () => {
   }
 
   const styles = {
-    modalContainerBg : "rgba(0, 0, 0, 0.7)",
-    bg : "#fff",
-    width : "200px",
-    height : "200px",
-    radius : "5px",
-    messageColor : "#000",
-    messageFontSize : "1em",
-    buttonColor : "#000",  
-    buttonBg : "#fff",
-    buttonWidth : "30px",
-    buttonHeight:"30px",
-    buttonHoverColor : "#fff",
-    buttonHoverBg: "#000"
-
-
+    modalContainerBg: 'rgba(0, 0, 0, 0.7)',
+    bg: '#fff',
+    width: '200px',
+    height: '200px',
+    radius: '5px',
+    messageColor: '#000',
+    messageFontSize: '1em',
+    buttonColor: '#000',
+    buttonBg: '#fff',
+    buttonWidth: '30px',
+    buttonHeight: '30px',
+    buttonHoverColor: '#fff',
+    buttonHoverBg: '#000'
   }
 
   return isShowing
     ? ReactDOM.createPortal(
         <>
-          <Modal 
-          modalStyles = {styles}
-
-          onClick={handleClickToClose} 
-
-          messageText="Employee created !"
-          
+          <Modal
+            modalStyles={styles}
+            onClick={handleClickToClose}
+            messageText='Employee created !'
           />
         </>,
         document.body
